4a3c5c600e55c88197ec19e1f28655ac0abd25e0
vivisuke <ntsuda@master.email.ne.jp>
2023-06-21 05:10:20 +0900

    0062 ◎ C 06/21 06/21 0.003│ToDo│Act分布シーン > インプットレイヤー：平均:0、分散:1 の100個のデータ生成＆（50個の点）表示

--- VisualFCL/scripts/activate_distri_scene.gd
+++ VisualFCL/scripts/activate_distri_scene.gd
@@ -1,11 +1,23 @@
-extends Node2D
+﻿extends Node2D
+
+const N_DATA = 100				# 入力データ数
+const N_NODE = 100				# 中間ノード数
+
+var vec_input = []				# 入力データ配列, [x, y, bool]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	init()
 	pass # Replace with function body.
-
-
+func init():
+	vec_input = []
+	for i in range(N_DATA/2):
+		var x = randfn(0.0, 1.0)
+		var y = randfn(0.0, 1.0)
+		vec_input.push_back([x, y, true])
+	$GraphRect_1.vec_input = vec_input
+	$GraphRect_1.maxv = 3.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
