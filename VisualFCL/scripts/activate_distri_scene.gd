7e42dfd90ee34345a87f032d67d4d8a8c0e59df3
vivisuke <ntsuda@master.email.ne.jp>
2023-06-24 11:20:17 +0900

    0069 ◎ C 06/22 06/24 0.003│ToDo│活性化関数選択可に（sigmoid, tanh, ReLU ?）

--- VisualFCL/scripts/activate_distri_scene.gd
+++ VisualFCL/scripts/activate_distri_scene.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 enum {		# 活性化関数種別
-	AF_SIGMOID = 0, AF_TANH, AF_RELU,
+	AF_TANH = 0, AF_SIGMOID, AF_RELU,
 }
 const N_INPUT = 50				# 入力データ数
 const N_NODE = 50				# 中間、出力ノード数
@@ -37,12 +37,14 @@ class Neuron:
 					vec_weight[k+1] *= cos(th)
 				vec_weight[i+2] = sin(th)
 	func sigmoid(x): return 1.0/(1.0 + exp(-x))
+	func ReLU(x): return x if x >= 0.0 else 0.0
 	func forward(inp: Array):
 		a = vec_weight[0]
 		for i in range(n_input):
 			a += vec_weight[i+1] * inp[i]
-		#y = sigmoid(a)
-		y = tanh(a)
+		if actv_func == AF_TANH: y = tanh(a)
+		elif actv_func == AF_SIGMOID: y = sigmoid(a)
+		elif actv_func == AF_RELU: y = ReLU(a)
 		#print("a = ", a, ", y = ", y)
 	func backward(inp: Array, grad: float):
 		upgrad = []		# 上流勾配
@@ -66,6 +68,9 @@ class FCLayer:
 		neuron_lst.resize(n_out)
 		for i in range(n_out):
 			neuron_lst[i] = Neuron.new(n_in, af, deviation)
+	func set_af(af):
+		for i in range(n_output):
+			neuron_lst[i].actv_func = af
 	func forward(inp: Array):
 		vec_output.resize(n_output)
 		for i in range(n_output):
@@ -105,7 +110,7 @@ func _ready():
 	#$GraphRect_6.to_plot_boolean = false
 	vec_layer.resize(N_LAYER)
 	for i in range(N_LAYER):
-		vec_layer[i] = FCLayer.new(N_INPUT, N_NODE, AF_TANH, 0.1)
+		vec_layer[i] = FCLayer.new(N_INPUT, N_NODE, AF_TANH, 0.1414)
 	#first_layer = FCLayer.new(N_INPUT, N_NODE, AF_TANH, 0.1)
 	#for i in range(N_NODE):
 	#	first_layer.push_back(FCN1Unit.new(N_INPUT, 0.1))
@@ -148,4 +153,11 @@ func _process(delta):
 
 func _on_top_button_pressed():
 	get_tree().change_scene_to_file("res://top_scene.tscn")
+	pass # Replace with function body.
+
+
+func _on_actv_func_button_item_selected(index):
+	for k in range(N_LAYER):
+		vec_layer[k].set_af(index)
+	forward()
 	pass # Replace with function body.
