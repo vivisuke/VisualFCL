b7845cca6e96064011fb3bf5b5ac2175696bdc31
vivisuke <ntsuda@master.email.ne.jp>
2023-06-30 08:55:30 +0900

    0094 ◎ C 06/30 06/30 0.004│ToDo│３層NN学習シーン：1-Layer に10個の入力（平均:0, 分散:1.0）表示

--- VisualFCL/scripts/fc3layer_scene.gd
+++ VisualFCL/scripts/fc3layer_scene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+var N_INPUT = 10
+var vec_pair = []				# データ配列, 要素：[x, y, bool]
 var vec_graph_rect = []
 
 # Called when the node enters the scene tree for the first time.
@@ -16,8 +17,19 @@ func _ready():
 		vec_graph_rect[i].add_child(lbl)
 		vec_graph_rect[i].to_draw_div_lines = true
 		vec_graph_rect[i].to_plot_boolean = false
+	init()
 	pass # Replace with function body.
-
+func init():
+	#vec_input = []
+	vec_pair = []
+	for i in range(N_INPUT):
+		var x1 = randfn(0.0, 1.0)
+		#vec_input.push_back(x)
+		var x2 = randfn(0.0, 1.0)
+		#vec_input.push_back(y)
+		vec_pair.push_back([x1, x2, false])
+	$GraphRect_1.vec_input = vec_pair
+	$GraphRect_1.maxv = 4.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
