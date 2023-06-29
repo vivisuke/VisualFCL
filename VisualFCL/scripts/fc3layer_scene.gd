57db825f18e85c006a91d20a0930cf9b8567f0d4
vivisuke <ntsuda@master.email.ne.jp>
2023-06-30 08:50:01 +0900

    0093 ◎ C 06/30 06/30 0.004│ToDo│３層NN学習シーン：GraphRect 上部に「N-Layer Input」表示

--- VisualFCL/scripts/fc3layer_scene.gd
+++ VisualFCL/scripts/fc3layer_scene.gd
@@ -1,8 +1,21 @@
-﻿extends Node2D
+extends Node2D
 
+
+var vec_graph_rect = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	vec_graph_rect.push_back($GraphRect_1)
+	vec_graph_rect.push_back($GraphRect_2)
+	vec_graph_rect.push_back($GraphRect_3)
+	for i in range(vec_graph_rect.size()):
+		var lbl = Label.new()
+		lbl.text = "%d-Layer Input" % (i+1)
+		lbl.add_theme_color_override("font_color", Color.BLACK)
+		lbl.position = Vector2(102, 0)
+		vec_graph_rect[i].add_child(lbl)
+		vec_graph_rect[i].to_draw_div_lines = true
+		vec_graph_rect[i].to_plot_boolean = false
 	pass # Replace with function body.
 
 
